38c2e22a016bb4ee925c31f4bb75fdea1f62c223
thales_hume <thales_hume@foxmail.com>
2023-01-07 23:42:54 +0800

    init

--- src/generator.ts
+++ src/generator.ts
@@ -1,9 +1,7 @@
 import { generatorHandler, GeneratorOptions } from '@prisma/generator-helper'
 import { logger } from '@prisma/sdk'
-import path from 'path'
 import { GENERATOR_NAME } from './constants'
-import { genEnum } from './helpers/genEnum'
-import { writeFileSafely } from './utils/writeFileSafely'
+import { generate } from './utils/generate'
 
 const { version } = require('../package.json')
 
@@ -16,16 +14,5 @@ generatorHandler({
       prettyName: GENERATOR_NAME,
     }
   },
-  onGenerate: async (options: GeneratorOptions) => {
-    options.dmmf.datamodel.enums.forEach(async (enumInfo) => {
-      const tsEnum = genEnum(enumInfo)
-
-      const writeLocation = path.join(
-        options.generator.output?.value!,
-        `${enumInfo.name}.ts`,
-      )
-
-      await writeFileSafely(writeLocation, tsEnum)
-    })
-  },
+  onGenerate: generate
 })
